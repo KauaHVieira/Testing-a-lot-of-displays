9abfab3349a6d847a659e15ead89b7c2642e5d02
KauaHVieira <161729999+KauaHVieira@users.noreply.github.com>
2024-09-12 15:05:09 +0000

    menupronto

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const navMenu =document.getElementById('nav-menu'),
-toggleMenu = document.getElementById('toggle-menu').
+toggleMenu = document.getElementById('toggle-menu'),
 closeMenu = document.getElementById('close-menu')
+
+toggleMenu.addEventListener('click', () => {
+navMenu.classList.toggle('show_menu')
+
+})
+
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show_menu')
+})
